5b9c38391e19d28044ce62d412e7f8b857058330
Brys Sepulveda <bksepulv@gmail.com>
2019-10-19 20:53:00 -0700

    Initial commit. Doesnt work. Mostly just a copy of the LOTR one. Need to go ahead and define the events then slap all the mods into rimworld

---
+++ Source/ToolkitAlienVsPredatorPatch.cs
@@ -0,0 +1,13 @@
+﻿using Verse;
+
+namespace ToolkitLordOfTheRimsPatch
+{
+    public class ToolkitAlienVsPredatorPatch : Mod
+    {
+        public ToolkitAlienVsPredatorPatch(ModContentPack content) : base(content)
+        {
+        }
+
+        public override string SettingsCategory() => "Toolkit AvP Patch";
+    }
+}
